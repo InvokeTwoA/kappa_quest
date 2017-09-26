cc6ebaab3c045d8615657ec771d099f26b793fb6
伊藤 直哉 <NaoyaIto@AdministratornoMacBook-Pro.local>
2017-09-26 16:56:38 +0900

    fix shopScene

--- kappa_quest/scenes/ShopScene.swift
+++ kappa_quest/scenes/ShopScene.swift
@@ -2,32 +2,33 @@ import SpriteKit
 import GameplayKit
 
 class ShopScene: BaseScene, JobDelegate {
-    
+
     private var page = 0
-    private var jobNameList0 = ["murabito", "wizard", "knight", "priest", "thief", "fighter"]
-    private var jobNameList1 = ["necro", "odoriko", "ninja", "", "", ""]
+    private var jobNameList0 = ["murabito", "wizard", "knight", "priest", "thief", "arakure"]
+    private var jobNameList1 = ["archer", "necro", "ninja", "samurai", "gundam", "fighter"]
+    private var jobNameList1 = ["king", "odoriko", "usagi", "sister", "assassin", "dark_knight"]
     private var enableJob : [Bool] = [true,false,false,false,false,false,false]
     private var jobList : [String]!
 
     override func sceneDidLoad() {
     }
-    
+
     override func didMove(to view: SKView) {
         setPageData()
     }
-    
+
     // delegate method
     func jobDidChanged() {
         setPageData()
     }
-    
+
     func setPageData(){
         jobModel.readDataByPlist()
         jobModel.loadParam()
         setJobData()
         updatePageButton()
     }
-    
+
     func setJobData(){
         if page == 0 {
             jobList = jobNameList0
@@ -38,7 +39,7 @@ class ShopScene: BaseScene, JobDelegate {
         for i in 0...5 {
             displayJobInfo(pos: i, job: jobList[i])
         }
-        
+
         for (index, key) in jobList.enumerated() {
             if key == jobModel.name {
                 changeBoxColor(index)
@@ -46,11 +47,8 @@ class ShopScene: BaseScene, JobDelegate {
         }
         setCurrentJobInfo()
     }
-    
+
     func displayJobInfo(pos : Int, job : String){
-        print(pos)
-        print(job)
-        
         let jobLvLabel  = childNode(withName: "//JobLv\(pos)") as! SKLabelNode
         let jobImageNode = childNode(withName: "//JobImage\(pos)") as! SKSpriteNode
 
@@ -87,21 +85,21 @@ class ShopScene: BaseScene, JobDelegate {
             break
         }
     }
-    
+
     func setJobInfo(pos : Int, job : String){
         let jobLvLabel  = childNode(withName: "//JobLv\(pos)") as! SKLabelNode
         let jobImageNode = childNode(withName: "//JobImage\(pos)") as! SKSpriteNode
         jobLvLabel.text = "LV \(JobModel.getLV(job))"
         jobImageNode.texture = SKTexture(imageNamed: job)
     }
-    
+
     func setHatenaImage(pos : Int){
         let jobLvLabel  = childNode(withName: "//JobLv\(pos)") as! SKLabelNode
         let jobImageNode = childNode(withName: "//JobImage\(pos)") as! SKSpriteNode
         jobLvLabel.text = ""
         jobImageNode.texture = SKTexture(imageNamed: "hatena")
     }
-    
+
     func setCurrentJobInfo(){
         let jobLvLabel      = childNode(withName: "//JobLvLabel") as! SKLabelNode
         let jobNameLabel    = childNode(withName: "//JobNameLabel") as! SKLabelNode
@@ -111,7 +109,7 @@ class ShopScene: BaseScene, JobDelegate {
         jobNameLabel.text = jobModel.displayName
         jobImage.texture = SKTexture(imageNamed: jobModel.name)
     }
-    
+
     func goNextPage(){
         page += 1
         updatePageButton()
@@ -125,13 +123,12 @@ class ShopScene: BaseScene, JobDelegate {
         changeBoxColor(7)
         setJobData()
     }
-    
+
     func updatePageButton(){
         let nextLabel  = childNode(withName: "//nextLabel") as? SKLabelNode
         let nextNode = childNode(withName: "//nextNode") as? SKSpriteNode
         let backLabel  = childNode(withName: "//backLabel") as? SKLabelNode
         let backNode = childNode(withName: "//backNode") as? SKSpriteNode
-        
         if page == 1 {
             nextLabel?.isHidden = true
             nextNode?.isHidden = true
@@ -144,13 +141,13 @@ class ShopScene: BaseScene, JobDelegate {
             backNode?.isHidden = true
         }
     }
-    
+
     func changeJob(_ position: Int) {
         let selected_job = jobList[position]
         if selected_job == "" || !enableJob[position] {
             return
         }
-        
+
         let storyboard = UIStoryboard(name: "Job", bundle: nil)
         let jobViewController = storyboard.instantiateViewController(withIdentifier: "JobViewController") as! JobViewController
         jobViewController.job = selected_job
@@ -158,19 +155,19 @@ class ShopScene: BaseScene, JobDelegate {
         jobViewController.delegate = self
         self.view?.window?.rootViewController?.present(jobViewController, animated: true, completion: nil)
     }
-    
+
     func changeBoxColor(_ position: Int) {
         for num in 0...5 {
             let box = self.childNode(withName: "//Job\(num)") as? SKShapeNode
             box?.fillColor = .white
         }
-        
+
         if position != 7 {
             let box = self.childNode(withName: "//Job\(position)") as? SKShapeNode
             box?.fillColor = .green
         }
     }
-    
+
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         for t in touches {
             let positionInScene = t.location(in: self)
@@ -179,7 +176,7 @@ class ShopScene: BaseScene, JobDelegate {
             if tapNode.name == nil {
                 return
             }
-            
+
             switch tapNode.name! {
             case "CloseNode", "CloseLabel":
                 goWorld()
